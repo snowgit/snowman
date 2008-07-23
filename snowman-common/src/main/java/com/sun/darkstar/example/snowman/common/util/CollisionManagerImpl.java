dee4be78f0184e0316c94da3d851edebb4b5070a
kbt <kbt@8b21c375-3f8a-4cfb-afe8-8be7742c0793>
2008-07-23 13:37:22 +0000

    Remove incorrect @overrides annotation

--- snowman-common/src/main/java/com/sun/darkstar/example/snowman/common/util/CollisionManagerImpl.java
+++ snowman-common/src/main/java/com/sun/darkstar/example/snowman/common/util/CollisionManagerImpl.java
@@ -154,7 +154,6 @@ public class CollisionManagerImpl implements CollisionManager
         return null;
     }
 
-    @Override
     public Vector3f getDestination(float x1, float z1, float x2, float z2, Spatial spatial) {
         //generate the start and destination points
         Vector3f start = new Vector3f(x1, 0.0f, z1);
@@ -201,7 +200,6 @@ public class CollisionManagerImpl implements CollisionManager
         }
     }
 
-    @Override
     public boolean validate(float x1, float z1, float x2, float z2, Spatial spatial) {
         // TODO Auto-generated method stub
         return false;
