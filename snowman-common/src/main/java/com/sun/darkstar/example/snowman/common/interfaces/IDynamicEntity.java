ee0e08e7dcc7b9f8361397ecb7f629ae214a3581
neakor <neakor@8b21c375-3f8a-4cfb-afe8-8be7742c0793>
2008-07-23 17:44:54 +0000

    Added resetForce() to IDynamicEntity.

--- snowman-common/src/main/java/com/sun/darkstar/example/snowman/common/interfaces/IDynamicEntity.java
+++ snowman-common/src/main/java/com/sun/darkstar/example/snowman/common/interfaces/IDynamicEntity.java
@@ -11,7 +11,7 @@ import com.jme.math.Vector3f;
  * 
  * @author Yi Wang (Neakor)
  * @version Creation date: 06-04-2008 14:54 EST
- * @version Modified date: 06-06-2008 16:10 EST
+ * @version Modified date: 07-23-2008 12:25 EST
  */
 public interface IDynamicEntity extends IEntity {
 	
@@ -20,6 +20,11 @@ public interface IDynamicEntity extends IEntity {
 	 * @param force The force in <code>Vector3f</code> form.
 	 */
 	public void addForce(Vector3f force);
+	
+	/**
+	 * Clear the force acting on this entity.
+	 */
+	public void resetForce();
 	
 	/**
 	 * Retrieve the mass value of this dynamic entity.
