457febd11cb48fe965ca140e5b461f957085144d
neakor <neakor@8b21c375-3f8a-4cfb-afe8-8be7742c0793>
2008-08-04 20:32:49 +0000

    Ported in animations and animation support.

--- snowman-client/src/main/java/com/sun/darkstar/example/snowman/data/enumn/EAnimation.java
+++ snowman-client/src/main/java/com/sun/darkstar/example/snowman/data/enumn/EAnimation.java
@@ -6,8 +6,35 @@ package com.sun.darkstar.example.snowman.data.enumn;
  * 
  * @author Yi Wang (Neakor)
  * @version Creation date: 06-10-2008 17:14 EST
- * @version Modified date: 06-10-2008 17:14 EST
+ * @version Modified date: 08-04-2008 12:32 EST
  */
 public enum EAnimation {
-
+	/**
+	 * The snowman idle animation.
+	 */
+	Idle,
+	/**
+	 * The snowman movement animation.
+	 */
+	Move,
+	/**
+	 * The snowman attack animation.
+	 */
+	Attack,
+	/**
+	 * The snowman hit animation.
+	 */
+	Hit,
+	/**
+	 * The snowman grabbing animation.
+	 */
+	Grabbing,
+	/**
+	 * The snowman death animation.
+	 */
+	Death,
+	/**
+	 * The snowman respawn animation.
+	 */
+	Respawn
 }
