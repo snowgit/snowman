293dfdc6d3530842ba837a122e28df30b409e907
renanse <renanse@8b21c375-3f8a-4cfb-afe8-8be7742c0793>
2008-08-20 18:01:54 +0000

    updated

--- snowman-client/src/main/java/com/sun/darkstar/example/snowman/data/util/export/ColladaExporter.java
+++ snowman-client/src/main/java/com/sun/darkstar/example/snowman/data/util/export/ColladaExporter.java
@@ -2,13 +2,9 @@ package com.sun.darkstar.example.snowman.data.util.export;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 
 import com.jme.input.KeyBindingManager;
 import com.jme.scene.Node;
-import com.jme.scene.SharedMesh;
-import com.jme.scene.Spatial;
-import com.jme.scene.TriMesh;
 import com.jmex.model.collada.ColladaImporter;
 import com.sun.darkstar.example.snowman.data.enumn.EDataType;
 
@@ -24,11 +20,11 @@ public class ColladaExporter extends Exporter {
 	/**
 	 * The source model file name without extension.
 	 */
-	private final String fileName = "Tree";
+	private final String fileName = "SnowGlobe";
 	/**
 	 * The mesh object to be exported.
 	 */
-	private TriMesh mesh;
+	private Node node;
 	
 	/**
 	 * Main method.
@@ -52,37 +48,16 @@ public class ColladaExporter extends Exporter {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		final ArrayList<TriMesh> store = new ArrayList<TriMesh>();
-		this.populateMeshes(ColladaImporter.getModel(), store);
-		if(store.size() > 1) System.out.println("There is more than one mesh.");
-		// There should be only one mesh.
-		this.mesh = store.get(0);
-		this.rootNode.attachChild(this.mesh);
+		this.node = ColladaImporter.getModel();
+		this.node.setLocalScale(.1f);  // Our Collada files were done in 100:1 scale
+		this.node.updateRenderState();
+		this.rootNode.attachChild(this.node);
 	}
-	
-	/**
-	 * Populate the given list with shared meshes from the given spatial.
-	 * @param spatial The <code>Spatial</code> to check <code>TriMesh</code> from.
-	 * @param store The <code>ArrayList</code> to store <code>TriMesh</code>.
-	 */
-	private void populateMeshes(Spatial spatial, ArrayList<TriMesh> store) {
-		if(spatial == null) return;
-		else if(spatial instanceof SharedMesh) {
-			store.add(((SharedMesh)spatial).getTarget());
-			return;
-		} else if(spatial instanceof Node) {
-			Node node = (Node)spatial;
-			if(node.getChildren() == null) return;
-			for(Spatial s : node.getChildren()) {
-				this.populateMeshes(s, store);
-			}
-		}
-	}
-	
+
 	@Override
 	protected void simpleUpdate() {
 		if(KeyBindingManager.getKeyBindingManager().isValidCommand("out", false)) {
-			this.export(this.fileName + EDataType.StaticMesh.getExtension(), this.mesh);
+			this.export(this.fileName + EDataType.StaticMesh.getExtension(), this.node);
 		}
 	}
 }
