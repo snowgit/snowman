b1441fa19e1d788ed11e43a7abb8f91a11d69ea8
renanse <renanse@8b21c375-3f8a-4cfb-afe8-8be7742c0793>
2008-08-28 20:51:50 +0000

    more icon data (urls to icon files added)

--- snowman-common/src/main/java/com/sun/darkstar/example/snowman/common/entity/enumn/ECursorState.java
+++ snowman-common/src/main/java/com/sun/darkstar/example/snowman/common/entity/enumn/ECursorState.java
@@ -1,5 +1,6 @@
 package com.sun.darkstar.example.snowman.common.entity.enumn;
 
+
 /**
  * <code>ECursorState</code> defines the enumerations of all the possible states
  * that the targeting cursor can be in.
@@ -10,17 +11,27 @@ public enum ECursorState {
     /**
      * Invalid operation
      */
-    Invalid,
+    Invalid("com/sun/darkstar/example/snowman/data/icons/IconWalking.png"),
     /**
      * The trying to move state
      */
-    TryingToMove,
+    TryingToMove("com/sun/darkstar/example/snowman/data/icons/IconWalking.png"),
     /**
      * The targeting other snowman state.
      */
-    Targeting,
+    Targeting("com/sun/darkstar/example/snowman/data/icons/IconTarget.png"),
     /**
      * The trying to grab flag state.
      */
-    TryingToGrab
+    TryingToGrab("com/sun/darkstar/example/snowman/data/icons/IconGrab.png");
+
+    final String iconLocation;
+    
+    private ECursorState(String location) {
+		this.iconLocation = location;
+	}
+    
+	public String getIconLocation() {
+		return iconLocation;
+	}
 }
