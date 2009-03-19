23f3bc20dc619b706706f076332e4fc1efd153c7
owenkellett <owenkellett@8b21c375-3f8a-4cfb-afe8-8be7742c0793>
2009-03-19 17:10:10 +0000

    update MatchmakerTask to new initial version

--- snowman-server/src/main/java/com/sun/darkstar/example/snowman/server/tasks/MatchmakerTask.java
+++ snowman-server/src/main/java/com/sun/darkstar/example/snowman/server/tasks/MatchmakerTask.java
@@ -42,7 +42,7 @@ import com.sun.sgs.app.Task;
 import com.sun.sgs.app.ManagedReference;
 import com.sun.sgs.app.ObjectNotFoundException;
 import java.io.Serializable;
-import java.util.Deque;
+import java.util.Queue;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -50,10 +50,11 @@ import java.util.logging.Logger;
 
 /**
  * This task is a self re-scheduling task that polls the front of the
- * login deques and uses the players to start a new game.
+ * login queue and uses the players to start a new game.
  * 
- * During each execution of the task, each individual deque will be
- * polled at most once.  The polled players will be added to a local
+ * During each execution of the task, the queue will be polled
+ * at most a number of times equal to the number of players
+ * per game.  The polled players will be added to a local
  * list of waiting players.  If the local list fills up, a game
  * will be started, the local list will be emptied,
  * and the matchmaker will be rescheduled.
@@ -74,113 +75,37 @@ public class MatchmakerTask implements Task, Serializable {
     
     private int gameCount = 0;
     private final int numPlayersPerGame;
-    private final int numRobotsPerGame;
-    private final int robotDelay;
     
     private GameFactory gameFactory;
     private EntityFactory entityFactory;
     
     private List<ManagedReference<SnowmanPlayer>> waitingPlayers;
-    private ManagedReference<Deque<ManagedReference<SnowmanPlayer>>>[] 
-            waitingDeques;
+    private ManagedReference<Queue<ManagedReference<SnowmanPlayer>>> waitingQueue;
     
     /**
      * Constructs a {@code MatchmakerTask} with the given attributes
      * and components.
      * 
      * @param numPlayersPerGame number of human players to include in a game
-     * @param numRobotsPerGame number of server controlled robots per game
-     * @param robotDelay configured delay between robot actions
      * @param gameFactory factory to create game objects
      * @param entityFactory factory to create in-game artifacts
-     * @param waitingDeques array of queues which contain connecting clients
+     * @param waitingQueue the queue which contain connecting clients
      */
     public MatchmakerTask(int numPlayersPerGame,
-                          int numRobotsPerGame,
-                          int robotDelay,
                           GameFactory gameFactory,
                           EntityFactory entityFactory,
-                          ManagedReference<Deque<ManagedReference<SnowmanPlayer>>>[] waitingDeques) {
+                          ManagedReference<Queue<ManagedReference<SnowmanPlayer>>> waitingQueue) {
         this.numPlayersPerGame = numPlayersPerGame;
-        this.numRobotsPerGame = numRobotsPerGame;
-        this.robotDelay = robotDelay;
         
         this.gameFactory = gameFactory;
         this.entityFactory = entityFactory;
         
         this.waitingPlayers = new ArrayList<ManagedReference<SnowmanPlayer>>();
-        this.waitingDeques = waitingDeques;
+        this.waitingQueue = waitingQueue;
     }
 
     /** {@inheritDoc} */
     public void run() throws Exception {
-        boolean playersFound = false;
-        //cycle through the front of each queue, adding players to the waiting
-        //list as they are found
-        for (int i = 0; i < waitingDeques.length; i++) {
-            ManagedReference<SnowmanPlayer> nextPlayer = 
-                    waitingDeques[i].get().poll();
-            if (nextPlayer != null) {
-                playersFound = true;
-                waitingPlayers.add(nextPlayer);
-            }
-            if (waitingPlayers.size() == numPlayersPerGame) {
-                startGame();
-                break;
-            }
-        }
-        
-        // if no players are found in the queue during this iteration
-        // schedule a delay for the next polling cycle
-        // otherwise, schedule the next cycle to occur immediately
-        if (playersFound) {
-            AppContext.getTaskManager().scheduleTask(this);
-        } else {
-            AppContext.getTaskManager().scheduleTask(this, POLLINGINTERVAL);
-        }
-    }
-    
-    private void startGame() {
-        //remove players from waiting list if they have disconnected
-        boolean needMore = false;
-        for (Iterator<ManagedReference<SnowmanPlayer>> ip =
-                waitingPlayers.iterator(); ip.hasNext(); ) {
-            try {
-                ip.next().get();
-            } catch (ObjectNotFoundException e) {
-                ip.remove();
-                needMore = true;
-            }
-        }
-        if (needMore) {
-            return;
-        }
-        
-        //start the game if all waiting players are still connected
-        String gameName = NAME_PREFIX + (gameCount++);
-        SnowmanGame game = 
-                gameFactory.createSnowmanGame(gameName,
-                                              numPlayersPerGame + 
-                                              numRobotsPerGame,
-                                              entityFactory);
-        ETeamColor color = ETeamColor.values()[0];
-        for (Iterator<ManagedReference<SnowmanPlayer>> ip = 
-                waitingPlayers.iterator(); ip.hasNext(); ) {
-            game.addPlayer(ip.next().get(), color);
-            color = ETeamColor.values()[(color.ordinal() + 1) % 
-                    ETeamColor.values().length];
-        }
-        for (int i = 0; i < numRobotsPerGame; i++) {
-            game.addPlayer(entityFactory.createRobotPlayer(gameName + 
-                                                           "_robot" + i,
-                                                           robotDelay),
-                           color);
-            color = ETeamColor.values()[(color.ordinal() + 1) % 
-                    ETeamColor.values().length];
-        }
-        game.sendMapInfo();
-        
-        //clear out the waiting list
-        waitingPlayers.clear();
+        // INSERT CODE HERE
     }
 }
